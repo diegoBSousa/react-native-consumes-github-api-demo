1365cc30bce8cd13a8cf08ef9dfee9597e35782b
Diego Sousa <diego@diegosousa.com.br>
2020-10-12 12:54:50 -0300

    Applies styles to main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {Text} from 'react-native';
+import {Container} from './styles';
 
 export default function Main() {
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <Container>
       <Text>Main Screen 1</Text>
-    </View>
+    </Container>
   );
 }
