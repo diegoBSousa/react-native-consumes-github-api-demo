d240ea38e65ff8836fb6fcd8c66b5fd5a6a9f189
Diego Sousa <diego@diegosousa.com.br>
2020-10-13 07:36:13 -0300

    Adds ActivityIndicator

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Keyboard} from 'react-native';
+import {Keyboard, ActivityIndicator} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import {
   Container,
@@ -17,13 +17,20 @@ import {
 import api from '../../services/api';
 
 export default class Main extends Component {
-  state = {
-    newUser: '',
-    users: [],
-  };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      newUser: '',
+      users: [],
+      loading: false,
+    };
+  }
 
   handleAddUser = async () => {
     const {newUser, users} = this.state;
+
+    this.setState({loading: true});
 
     const response = await api.get(`/users/${newUser}`);
 
@@ -37,13 +44,14 @@ export default class Main extends Component {
     this.setState({
       users: [...users, data],
       newUser: '',
+      loading: false,
     });
 
     Keyboard.dismiss();
   };
 
   render() {
-    const {users, newUser} = this.state;
+    const {users, newUser, loading} = this.state;
     return (
       <Container>
         <Form>
@@ -56,8 +64,12 @@ export default class Main extends Component {
             returnKeyType="send"
             onSubmitEditing={this.handleAddUser}
           />
-          <SubmitButton onPress={this.handleAddUser}>
-            <Icon name="add" size={20} color="#fff" />
+          <SubmitButton loading={loading} onPress={this.handleAddUser}>
+            {loading ? (
+              <ActivityIndicator color="#fff" />
+            ) : (
+              <Icon name="add" size={20} color="#fff" />
+            )}
           </SubmitButton>
         </Form>
         <List
