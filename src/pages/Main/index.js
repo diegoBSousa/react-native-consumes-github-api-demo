72f698b4b14cbcdf63601a5f0b125470c9dc1151
Diego Sousa <diego@diegosousa.com.br>
2020-10-13 06:31:27 -0300

    Calls github api

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import {Container, Form, Input, SubmitButton} from './styles';
+import api from '../../services/api';
 
 export default class Main extends Component {
   state = {
@@ -8,8 +9,22 @@ export default class Main extends Component {
     users: [],
   };
 
-  handleAddUser = () => {
-    console.tron.log(this.state.newUser);
+  handleAddUser = async () => {
+    const {newUser, users} = this.state;
+
+    const response = await api.get(`/users/${newUser}`);
+
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url,
+    };
+
+    this.setState({
+      users: [...users, data],
+      newUser: '',
+    });
   };
 
   render() {
