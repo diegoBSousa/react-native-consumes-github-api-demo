b82cf0766c17bb735507079c5a5d5f48fcddf90b
Diego Sousa <diego@diegosousa.com.br>
2020-10-15 12:29:31 -0300

    Get starred repositories from api.

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, {useLayoutEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 import {View} from 'react-native';
+import api from '../../services/api';
 
-export default function User({navigation, route}) {
-  console.tron.log(navigation);
-  console.tron.log(route);
-  console.tron.log(route.params.user);
-  console.tron.log(this);
+async function getStarredRepositories(user) {
+  return await api.get(`/users/${user}/starred`);
+}
+
+function User({navigation, route}) {
+  const [name, setName] = useState(route.params.user.name);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: name === '' ? 'Name Not Found' : name,
+    });
+  }, [navigation, name]);
+
+  const response = getStarredRepositories(route.params.user.login);
+
+  if (typeof response.data === 'array') {
+    const [stars, _] = useState(response.data);
+  }
+
   return <View />;
 }
+
+User.PropTypes = {
+  navigation: PropTypes.shape({
+    setOptions: PropTypes.func.isRequired,
+  }).isRequired,
+  route: PropTypes.shape({
+    params: PropTypes.shape({
+      user: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        login: PropTypes.string.isRequired,
+      }).isRequired,
+    }).isRequired,
+  }).isRequired,
+};
+
+export default User;
