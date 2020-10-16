4efc64734a809b8700fc11957ddd7bf7c5201ce0
Diego Sousa <diego@diegosousa.com.br>
2020-10-16 07:41:13 -0300

    Adds styled componentes to User page.

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -2,13 +2,15 @@ import React, {useLayoutEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 import {View} from 'react-native';
 import api from '../../services/api';
+import {Container, Header, Avatar, Name, Bio} from './styles';
 
 async function getStarredRepositories(user) {
   return await api.get(`/users/${user}/starred`);
 }
 
 function User({navigation, route}) {
-  const [name, setName] = useState(route.params.user.name);
+  const {user} = route.params;
+  const [name, setName] = useState(user.name);
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -16,27 +18,35 @@ function User({navigation, route}) {
     });
   }, [navigation, name]);
 
-  const response = getStarredRepositories(route.params.user.login);
+  const response = getStarredRepositories(user.login);
 
   if (typeof response.data === 'array') {
     const [stars, _] = useState(response.data);
   }
 
-  return <View />;
+  return (
+    <Container>
+      <Header>
+        <Avatar source={{uri: user.avatar}} />
+        <Name>{user.name}</Name>
+        <Bio>{user.bio}</Bio>
+      </Header>
+    </Container>
+  );
 }
 
-User.PropTypes = {
-  navigation: PropTypes.shape({
-    setOptions: PropTypes.func.isRequired,
-  }).isRequired,
-  route: PropTypes.shape({
-    params: PropTypes.shape({
-      user: PropTypes.shape({
-        name: PropTypes.string.isRequired,
-        login: PropTypes.string.isRequired,
-      }).isRequired,
-    }).isRequired,
-  }).isRequired,
-};
+// User.PropTypes = {
+//  navigation: PropTypes.shape({
+//    setOptions: PropTypes.func.isRequired,
+//  }).isRequired,
+//  route: PropTypes.shape({
+//    params: PropTypes.shape({
+//      user: PropTypes.shape({
+//        name: PropTypes.string.isRequired,
+//        login: PropTypes.string.isRequired,
+//      }).isRequired,
+//    }).isRequired,
+//  }).isRequired,
+// };
 
 export default User;
